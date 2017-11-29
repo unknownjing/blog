e5ac0b66f843c5ca76255953285c846ccbac788d
unknownjing <shaojingcai@126.com>
2017-11-29 15:24:00 +0800

    11a

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ from flask import Flask
 from config import DevConfig
 
 app = Flask(__name__)
-
+# Import the views module
+views = __import__('views')
 # Get the config from object of DecConfig
 # 使用 onfig.from_object() 而不使用 app.config['DEBUG'] 是因为这样可以加载 class DevConfig 的配置变量集合，而不需要一项一项的添加和修改。
 app.config.from_object(DevConfig)
